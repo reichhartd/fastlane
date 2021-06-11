f27d332591ed6d57165d7b9a744808baa885ab61
Daniel Reichhart <daniel.reichhart@gmx.net>
2021-06-11 14:39:49 +0200

    Added author

--- fastlane/lib/fastlane/actions/increment_version_name.rb
+++ fastlane/lib/fastlane/actions/increment_version_name.rb
@@ -61,8 +61,7 @@ module Fastlane
       end
 
       def self.authors
-        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
-        ["Your GitHub/Twitter Name"]
+        ["reichhartd"]
       end
 
       def self.is_supported?(platform)
